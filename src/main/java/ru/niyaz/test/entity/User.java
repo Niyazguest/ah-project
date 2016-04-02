476904709d1df169052c1d62b63cf4e8a7adf155
Niyazguest <NiyazKazan@yandex.ru>
2016-04-02 16:56:24 +0300

    Init

---
+++ src/main/java/ru/niyaz/test/entity/User.java
@@ -0,0 +1,67 @@
+package ru.niyaz.test.entity;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by user on 04.09.15.
+ */
+
+@Entity
+@Table(name = "user", schema = "public")
+public class User {
+    private Long userId;
+    private String name;
+    private String login;
+    private String password;
+    private Integer role;
+
+    @Id
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_user_id_seq", allocationSize = 0)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @Column(name = "user_id")
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "role")
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+}
